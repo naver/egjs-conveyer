3b0ebc761e82dafa0683c134d3646a589f653243
youjin-ahn <youjin.ahn@navercorp.com>
2023-09-19 17:59:01 +0900

    docs: modify Reactive Properties docs (js)

--- packages/conveyer/src/types.ts
+++ packages/conveyer/src/types.ts
@@ -78,8 +78,8 @@ export interface ConveyerOptions {
    */
   nested?: boolean;
   /**
-   * Whether to use the {@link https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver} when resizing the Conveyer element. (IE unsupported)
-   * <ko>Conveyer의 element 크기가 변경될 때 {@link https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver} 적용 여부 (IE 미지원) </ko>
+   * Whether to use the {@link https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver ResizeObserver} when resizing the Conveyer element.
+   * <ko>Conveyer의 element 크기가 변경될 때 {@link https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver ResizeObserver} 적용 여부 </ko>
    * @default false
    */
   useResizeObserver?: boolean;
