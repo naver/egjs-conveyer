3ef57e151be9271127489e2dedd28cb5d523ccae
malangfox <seonghyun.ahn@navercorp.com>
2025-08-27 16:58:46 +0900

    refactor: use pnpm and vite for packages

---
+++ packages/conveyer/vite.config.ts
@@ -0,0 +1,73 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
+
+const pkg = require("./package.json");
+
+const banner = `/*!
+ * ${pkg.name} v${pkg.version}
+ * (c) ${new Date().getFullYear()} ${pkg.author}
+ * @license ${pkg.license}
+ */`;
+
+const format = process.env.VITE_BUILD_FORMAT;
+
+export default defineConfig({
+  plugins: [
+    visualizer({
+      sourcemap: true,
+      filename: "./statistics/index.html",
+      title: `${pkg.name} bundle stats`,
+    }),
+  ],
+  define: {
+    "#__VERSION__#": JSON.stringify(pkg.version),
+    "#__FILETYPE__#": JSON.stringify(format || "unknown"),
+  },
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: path.resolve(__dirname, `src/index${format === "umd" || format === "cjs" ? "." + format : ""}.ts`),
+      name: "Conveyer",
+      formats: [format],
+      fileName: (format) => {
+        if (process.env.VITE_BUILD_MINIFY) {
+          return `conveyer.min.js`;
+        }
+        const formatExt = {
+          "umd": "js",
+          "cjs": "cjs.js",
+          "es": "esm.js",
+        };
+        return `conveyer.${formatExt[format]}`;
+      },
+    },
+    sourcemap: true,
+    minify: process.env.VITE_BUILD_MINIFY ? "terser" : false,
+    terserOptions: process.env.VITE_BUILD_MINIFY ? {
+      compress: {
+        drop_console: true,
+      },
+      format: {
+        comments: (node, comment) => {
+          const text = comment.value;
+          const type = comment.type;
+          if (type === "comment2") {
+            return new RegExp(pkg.name, "i").test(text) || /license/i.test(text);
+          }
+        },
+      },
+    } : {},
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {},
+        banner,
+        freeze: false,
+        exports: format === "umd" ? "default" : "named",
+      },
+    },
+    outDir: "dist",
+  },
+});
